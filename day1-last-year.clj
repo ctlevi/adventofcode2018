6e9452b28f261a32bb9d1b5fb90605302f435c84
Chris Tate <ctlevi@gmail.com>
2018-12-01 11:15:13 -0500

    Advent of code 2018 day 1 with some practice too

---
+++ day1-last-year.clj
@@ -0,0 +1,38 @@
+; (def inputNumbers [5 2 2 5 5 7])
+(def inputNumbers
+  (->>
+   (slurp "day1-last-year.txt")
+   (vec)
+   (mapv str)
+   (mapv #(Integer/parseInt %))))
+
+(def zipped
+  (->>
+   (conj
+    (subvec inputNumbers 1)
+    (first inputNumbers))
+   (mapv vector inputNumbers)))
+
+(def answer1
+  (->>
+   (filterv (fn [[x y]] (= x y)) zipped)
+   (mapv (fn [[x]] x))
+   (reduce +)))
+
+(def answer2
+  (->>
+   (split-at (/ (count inputNumbers) 2) inputNumbers)
+   ; This performs a zip of the split done above
+   (apply mapv vector)
+   (filterv (fn [[x y]] (= x y)))
+   ; Since the list wraps around, and we are considering the halfway around index of each element, we can assume
+   ; that an index at i has a wrap around value of j and j has a wrap around value of i.
+   ; x and y are the same, I could have done (* x 2) for instance
+   (mapv (fn [[x y]] (+ x y)))
+   (reduce +)))
+
+(println answer1)
+(println answer2)
+
+; (println (solution inputNumbers))
+; (println zipped)
---
+++ day1.clj
@@ -0,0 +1,20 @@
+; (def inputNumbers [7 7 -2 -7 -4])
+(def inputNumbers
+  (->>
+   (slurp "day1.txt")
+   (clojure.string/split-lines)
+   (mapv #(Integer/parseInt %))))
+
+(def answer1
+  (reduce + inputNumbers))
+
+(def answer2
+  (let [frequencyChanges (reductions + (cycle inputNumbers))]
+    (loop [[first & rest] frequencyChanges
+           seen #{0}]
+      (if (contains? seen first)
+        first
+        (recur rest (conj seen first))))))
+
+(println answer1)
+(println answer2)
