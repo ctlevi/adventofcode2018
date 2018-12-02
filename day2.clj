1bef9bdc96f2caf5df2d7536134f3920921e4d1b
Chris Tate <ctlevi@gmail.com>
2018-12-02 11:00:24 -0500

    Implement day2 part 2

--- day2.clj
+++ day2.clj
@@ -1,4 +1,5 @@
 ; (def input ["abcdef" "bababc" "abbcde" "abcccd" "aabcdd" "abcdee" "ababab"])
+; (def input ["abcde" "fghij" "klmno" "pqrst" "fguij" "axcye" "wvxyz"])
 (def input
   (->>
    (slurp "day2.txt")
@@ -24,5 +25,33 @@
    ; Multiple :two and :three together. Probably a much easier way...
    (reduce (fn [total [k v]] (* total v)) 1)))
 
+(defn generatePairsFromIndex [coll index]
+  (let [element (get coll index)]
+    (map #(vector element %) (nthrest coll (+ index 1)))))
+(defn generatePairs [coll]
+  (mapcat (partial generatePairsFromIndex coll) (range (count coll))))
+
+(defn differBy [first second]
+  (->>
+   (mapv vector first second)
+   (reduce (fn [sum [x y]] (if (not= x y) (inc sum) sum)) 0)))
+
+(defn sameLetters [first second]
+  (->>
+   (mapv vector first second)
+   (reduce (fn [same [x y]] (if (= x y) (conj same x) same)) [])
+   (clojure.string/join "")))
+
+(defn returnPairIfDifferBy [amount [first second :as pair]]
+  (if (= (differBy first second) amount) pair nil))
+
+(def answer2
+  (->>
+   (generatePairs input)
+   (map (partial returnPairIfDifferBy 1))
+   (filter not-empty)
+   (first)
+   (apply sameLetters)))
+
 (println answer1)
-; (println answer2)
+(println answer2)
