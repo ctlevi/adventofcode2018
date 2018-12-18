98f8fc678de86081a6d0b5918fd5e0bec75824ee
Chris Tate <ctlevi@gmail.com>
2018-12-18 09:13:24 -0500

    Use Clojure v1.10

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"] [org.clojure/core.rrb-vector "0.0.12"] [org.clojure/data.finger-tree "0.0.3"]])
+  :dependencies [[org.clojure/clojure "1.10.0"] [org.clojure/core.rrb-vector "0.0.12"] [org.clojure/data.finger-tree "0.0.3"]])
